9af965cb7b24d2831b7a47a0210650e7012d8bc3
NIGHT-GAMES-20 <night20games@gmail.com>
2025-07-20 12:29:03 +0530

    Updated get-user.js

---
+++ netlify/functions/get-user.js
@@ -0,0 +1,15 @@
+import dotenv from "dotenv";
+import { DataAPIClient } from "@datastax/astra-db-ts";
+
+dotenv.config()
+
+try{
+  const AstraClient = new DataAPIClient(process.env.VITE_ASTRA_DB_TOKEN);
+  const AstraDB = AstraClient.db(process.env.VITE_ASTRA_DB_URL);
+  
+  const collection =  AstraDB.collection("notes")
+  
+  console.log(await collection.findOne({}))
+}catch(err){
+  console.log("err : ", err)
+}
